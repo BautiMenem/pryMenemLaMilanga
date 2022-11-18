98e9a69d6f8cf96ece9827936c5219d080640caf
54351 <54351@DESKTOP-LHKS2HT>
2022-11-18 17:09:16 -0300

    Ultimo

--- FrmMain.Designer.cs
+++ FrmMain.Designer.cs
@@ -28,21 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
             this.dtvMozos = new System.Windows.Forms.DataGridView();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnTotales = new System.Windows.Forms.Button();
+            this.txtBebidasSinAlc = new System.Windows.Forms.TextBox();
+            this.txtComidas = new System.Windows.Forms.TextBox();
+            this.txtBebidasConAlc = new System.Windows.Forms.TextBox();
+            this.txtPostres = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.lblBebidasSinAlc = new System.Windows.Forms.Label();
+            this.lblbebidasConAlc = new System.Windows.Forms.Label();
+            this.lblComidas = new System.Windows.Forms.Label();
+            this.lblPostres = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnMozo = new System.Windows.Forms.Button();
+            this.txtMozoDelDia = new System.Windows.Forms.TextBox();
+            this.txtMontoTotal = new System.Windows.Forms.TextBox();
+            this.lblMonto = new System.Windows.Forms.Label();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Postres = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.btnValidar = new System.Windows.Forms.Button();
-            this.btnSalir = new System.Windows.Forms.Button();
-            this.btnTotal = new System.Windows.Forms.Button();
-            this.lblTotal = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dtvMozos)).BeginInit();
             this.SuspendLayout();
             // 
             // dtvMozos
             // 
+            this.dtvMozos.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
             this.dtvMozos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dtvMozos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
@@ -50,92 +65,215 @@
             this.Column3,
             this.Column4,
             this.Postres});
-            this.dtvMozos.Location = new System.Drawing.Point(24, 23);
+            this.dtvMozos.GridColor = System.Drawing.SystemColors.InactiveCaption;
+            this.dtvMozos.Location = new System.Drawing.Point(12, 23);
             this.dtvMozos.Name = "dtvMozos";
-            this.dtvMozos.Size = new System.Drawing.Size(542, 160);
+            this.dtvMozos.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dtvMozos.Size = new System.Drawing.Size(542, 175);
             this.dtvMozos.TabIndex = 0;
             this.dtvMozos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dtvMozos.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dtvMozos_KeyPress);
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(457, 204);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(97, 24);
+            this.btnValidar.TabIndex = 3;
+            this.btnValidar.Text = "Validar Datos";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(457, 339);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(97, 19);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnTotales
+            // 
+            this.btnTotales.Location = new System.Drawing.Point(12, 301);
+            this.btnTotales.Name = "btnTotales";
+            this.btnTotales.Size = new System.Drawing.Size(92, 19);
+            this.btnTotales.TabIndex = 5;
+            this.btnTotales.Text = "Totales";
+            this.btnTotales.UseVisualStyleBackColor = true;
+            this.btnTotales.Click += new System.EventHandler(this.btnTotales_Click);
+            // 
+            // txtBebidasSinAlc
+            // 
+            this.txtBebidasSinAlc.Location = new System.Drawing.Point(12, 339);
+            this.txtBebidasSinAlc.Name = "txtBebidasSinAlc";
+            this.txtBebidasSinAlc.Size = new System.Drawing.Size(60, 20);
+            this.txtBebidasSinAlc.TabIndex = 6;
+            // 
+            // txtComidas
+            // 
+            this.txtComidas.Location = new System.Drawing.Point(189, 339);
+            this.txtComidas.Name = "txtComidas";
+            this.txtComidas.Size = new System.Drawing.Size(60, 20);
+            this.txtComidas.TabIndex = 7;
+            this.txtComidas.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtBebidasConAlc
+            // 
+            this.txtBebidasConAlc.Location = new System.Drawing.Point(102, 339);
+            this.txtBebidasConAlc.Name = "txtBebidasConAlc";
+            this.txtBebidasConAlc.Size = new System.Drawing.Size(60, 20);
+            this.txtBebidasConAlc.TabIndex = 7;
+            // 
+            // txtPostres
+            // 
+            this.txtPostres.Location = new System.Drawing.Point(273, 339);
+            this.txtPostres.Name = "txtPostres";
+            this.txtPostres.Size = new System.Drawing.Size(60, 20);
+            this.txtPostres.TabIndex = 8;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(355, 339);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(60, 20);
+            this.txtTotal.TabIndex = 9;
+            // 
+            // lblBebidasSinAlc
+            // 
+            this.lblBebidasSinAlc.AutoSize = true;
+            this.lblBebidasSinAlc.Location = new System.Drawing.Point(9, 323);
+            this.lblBebidasSinAlc.Name = "lblBebidasSinAlc";
+            this.lblBebidasSinAlc.Size = new System.Drawing.Size(76, 13);
+            this.lblBebidasSinAlc.TabIndex = 10;
+            this.lblBebidasSinAlc.Text = "Bebidas s/ Alc";
+            // 
+            // lblbebidasConAlc
+            // 
+            this.lblbebidasConAlc.AutoSize = true;
+            this.lblbebidasConAlc.Location = new System.Drawing.Point(99, 323);
+            this.lblbebidasConAlc.Name = "lblbebidasConAlc";
+            this.lblbebidasConAlc.Size = new System.Drawing.Size(77, 13);
+            this.lblbebidasConAlc.TabIndex = 11;
+            this.lblbebidasConAlc.Text = "Bebidas c/ Alc";
+            // 
+            // lblComidas
+            // 
+            this.lblComidas.AutoSize = true;
+            this.lblComidas.Location = new System.Drawing.Point(186, 323);
+            this.lblComidas.Name = "lblComidas";
+            this.lblComidas.Size = new System.Drawing.Size(47, 13);
+            this.lblComidas.TabIndex = 12;
+            this.lblComidas.Text = "Comidas";
+            // 
+            // lblPostres
+            // 
+            this.lblPostres.AutoSize = true;
+            this.lblPostres.Location = new System.Drawing.Point(270, 323);
+            this.lblPostres.Name = "lblPostres";
+            this.lblPostres.Size = new System.Drawing.Size(42, 13);
+            this.lblPostres.TabIndex = 13;
+            this.lblPostres.Text = "Postres";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(352, 323);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(31, 13);
+            this.lblTotal.TabIndex = 14;
+            this.lblTotal.Text = "Total";
+            // 
+            // btnMozo
+            // 
+            this.btnMozo.Location = new System.Drawing.Point(12, 207);
+            this.btnMozo.Name = "btnMozo";
+            this.btnMozo.Size = new System.Drawing.Size(92, 21);
+            this.btnMozo.TabIndex = 15;
+            this.btnMozo.Text = "Mozo del Día";
+            this.btnMozo.UseVisualStyleBackColor = true;
+            this.btnMozo.Click += new System.EventHandler(this.btnMozo_Click);
+            // 
+            // txtMozoDelDia
+            // 
+            this.txtMozoDelDia.Location = new System.Drawing.Point(110, 208);
+            this.txtMozoDelDia.Name = "txtMozoDelDia";
+            this.txtMozoDelDia.Size = new System.Drawing.Size(82, 20);
+            this.txtMozoDelDia.TabIndex = 16;
+            // 
+            // txtMontoTotal
+            // 
+            this.txtMontoTotal.Location = new System.Drawing.Point(242, 208);
+            this.txtMontoTotal.Name = "txtMontoTotal";
+            this.txtMontoTotal.Size = new System.Drawing.Size(82, 20);
+            this.txtMontoTotal.TabIndex = 17;
+            // 
+            // lblMonto
+            // 
+            this.lblMonto.AutoSize = true;
+            this.lblMonto.Location = new System.Drawing.Point(199, 214);
+            this.lblMonto.Name = "lblMonto";
+            this.lblMonto.Size = new System.Drawing.Size(37, 13);
+            this.lblMonto.TabIndex = 18;
+            this.lblMonto.Text = "Monto";
             // 
             // Column1
             // 
             this.Column1.FillWeight = 4F;
             this.Column1.HeaderText = "Mozos";
             this.Column1.Name = "Column1";
-            this.Column1.ReadOnly = true;
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // Column2
             // 
             this.Column2.HeaderText = "Bebidas Sin Alcohol";
             this.Column2.Name = "Column2";
-            this.Column2.ReadOnly = true;
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // Column3
             // 
             this.Column3.HeaderText = "Bebidas Con Alcohol";
             this.Column3.Name = "Column3";
-            this.Column3.ReadOnly = true;
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // Column4
             // 
             this.Column4.HeaderText = "Comidas";
             this.Column4.Name = "Column4";
-            this.Column4.ReadOnly = true;
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // Postres
             // 
             this.Postres.HeaderText = "Postres";
             this.Postres.Name = "Postres";
-            this.Postres.ReadOnly = true;
-            // 
-            // btnValidar
-            // 
-            this.btnValidar.Location = new System.Drawing.Point(469, 199);
-            this.btnValidar.Name = "btnValidar";
-            this.btnValidar.Size = new System.Drawing.Size(97, 24);
-            this.btnValidar.TabIndex = 3;
-            this.btnValidar.Text = "Validar Datos";
-            this.btnValidar.UseVisualStyleBackColor = true;
-            // 
-            // btnSalir
-            // 
-            this.btnSalir.Location = new System.Drawing.Point(24, 266);
-            this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(75, 19);
-            this.btnSalir.TabIndex = 4;
-            this.btnSalir.Text = "Salir";
-            this.btnSalir.UseVisualStyleBackColor = true;
-            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
-            // 
-            // btnTotal
-            // 
-            this.btnTotal.Location = new System.Drawing.Point(383, 266);
-            this.btnTotal.Name = "btnTotal";
-            this.btnTotal.Size = new System.Drawing.Size(75, 19);
-            this.btnTotal.TabIndex = 5;
-            this.btnTotal.Text = "Total";
-            this.btnTotal.UseVisualStyleBackColor = true;
-            // 
-            // lblTotal
-            // 
-            this.lblTotal.AutoSize = true;
-            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotal.Location = new System.Drawing.Point(464, 260);
-            this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(18, 25);
-            this.lblTotal.TabIndex = 6;
-            this.lblTotal.Text = ".";
-            this.lblTotal.Click += new System.EventHandler(this.lblTotal_Click);
+            this.Postres.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(581, 310);
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(566, 371);
+            this.Controls.Add(this.lblMonto);
+            this.Controls.Add(this.txtMontoTotal);
+            this.Controls.Add(this.txtMozoDelDia);
+            this.Controls.Add(this.btnMozo);
             this.Controls.Add(this.lblTotal);
-            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.lblPostres);
+            this.Controls.Add(this.lblComidas);
+            this.Controls.Add(this.lblbebidasConAlc);
+            this.Controls.Add(this.lblBebidasSinAlc);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtPostres);
+            this.Controls.Add(this.txtBebidasConAlc);
+            this.Controls.Add(this.txtComidas);
+            this.Controls.Add(this.txtBebidasSinAlc);
+            this.Controls.Add(this.btnTotales);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnValidar);
             this.Controls.Add(this.dtvMozos);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmMain";
@@ -153,8 +291,21 @@
         private System.Windows.Forms.DataGridView dtvMozos;
         private System.Windows.Forms.Button btnValidar;
         private System.Windows.Forms.Button btnSalir;
-        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Button btnTotales;
+        private System.Windows.Forms.TextBox txtBebidasSinAlc;
+        private System.Windows.Forms.TextBox txtComidas;
+        private System.Windows.Forms.TextBox txtBebidasConAlc;
+        private System.Windows.Forms.TextBox txtPostres;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label lblBebidasSinAlc;
+        private System.Windows.Forms.Label lblbebidasConAlc;
+        private System.Windows.Forms.Label lblComidas;
+        private System.Windows.Forms.Label lblPostres;
         private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnMozo;
+        private System.Windows.Forms.TextBox txtMozoDelDia;
+        private System.Windows.Forms.TextBox txtMontoTotal;
+        private System.Windows.Forms.Label lblMonto;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
--- FrmMain.cs
+++ FrmMain.cs
@@ -15,8 +15,9 @@ namespace pryMenemLaMilanga
     public partial class FrmMain : Form
     {
 
-        
-        
+        public int[,] MatrizDatos = new int[5, 4];
+        public int[] VectorDatos = new int[10];
+
         public FrmMain()
         {
             InitializeComponent();
@@ -30,6 +31,7 @@ namespace pryMenemLaMilanga
             dtvMozos.Rows.Add("Javier");
             dtvMozos.Rows.Add("Gonzalo");
             dtvMozos.Rows.Add("Andres");
+            dtvMozos.Columns[0].ReadOnly = true;
         }
 
         private void btnSalir_Click(object sender, EventArgs e)
@@ -37,24 +39,170 @@ namespace pryMenemLaMilanga
             this.Close();
         }
 
-        private void lblTotal_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
 
-        private void btnAgregar_Click(object sender, EventArgs e)
+       
+
+       
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-                   
+
         }
 
-        private void txtNombres_TextChanged(object sender, EventArgs e)
+        private void btnValidar_Click(object sender, EventArgs e)
         {
+            int i = 0;
+            int f = 0;
+            int c = 1;
+            int cmat = 0;
+            while (f <= 4)
+            {
+                while (c <= 4)
+                {
 
+                    if (dtvMozos.Rows[f].Cells[c].Value == null || dtvMozos.Rows[f].Cells[c].Value == "")
+                    {
+                        i++;
+                    }
+                    c++;
+                }
+                c = 1;
+                f++;
+            }
+
+            if (i == 0)
+            {
+
+                btnMozo.Enabled = true;
+                btnTotales.Enabled = true;
+
+                f = 0;
+                c = 1;
+
+                while (f <= 4)
+                {
+                    while (c <= 4 && cmat <= MatrizDatos.GetLength(1))
+                    {
+                        MatrizDatos[f, cmat] = Convert.ToInt32(dtvMozos.Rows[f].Cells[c].Value);
+                        c++;
+                        cmat++;
+                    }
+                    c = 1;
+                    cmat = 0;
+                    f++;
+                }
+            }
+            else
+            {
+
+                btnMozo.Enabled = false;
+                btnTotales.Enabled = false;
+                MessageBox.Show("Asegurese de ingresar valores numéricos.", "Error de Carga", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnMozo_Click(object sender, EventArgs e)
+        {
+            int c = 0;
+            int f = 0;
+            int Suma = 0;
+            int Mayor = 0;
+            int Posicion = 0;
+            while (f < MatrizDatos.GetLength(0))
+            {
+                while (c < MatrizDatos.GetLength(1))
+                {
+                    Suma = Suma + MatrizDatos[f, c];
+                    c++;
+                }
+                VectorDatos[f] = Suma;
+                c = 0;
+                f++;
+                Suma = 0;
+            }
+
+            f = 0;
+            Mayor = VectorDatos[0];
+            while (f <= 4)
+            {
+                if (VectorDatos[f] >= Mayor)
+                {
+                    Mayor = VectorDatos[f];
+                    Posicion = f;
+                }
+                f++;
+            }
+
+            if (Posicion == 0)
+            {
+                txtMozoDelDia.Text = "Julio";
+                txtMontoTotal.Text = Mayor.ToString();
+            }
+            if (Posicion == 1)
+            {
+                txtMozoDelDia.Text = "Esteban";
+                txtMontoTotal.Text = Mayor.ToString();
+            }
+            if (Posicion == 2)
+            {
+                txtMozoDelDia.Text = "Javier";
+                txtMontoTotal.Text = Mayor.ToString();
+            }
+            if (Posicion == 3)
+            {
+                txtMozoDelDia.Text = "Gonzalo";
+                txtMontoTotal.Text = Mayor.ToString();
+            }
+            if (Posicion == 4)
+            {
+                txtMozoDelDia.Text = "Andres";
+                txtMontoTotal.Text = Mayor.ToString();
+            }
+        }
+
+        private void btnTotales_Click(object sender, EventArgs e)
+        {
+            int f = 0;
+            int c = 0;
+            int i = 5;
+            int Suma = 0;
+            int Total = 0;
+
+            while (c < MatrizDatos.GetLength(1) && i <= 9)
+            {
+                Suma = 0;
+                while (f < MatrizDatos.GetLength(0))
+                {
+                    Suma = Suma + MatrizDatos[f, c];
+                    f++;
+                }
+                VectorDatos[i] = Suma;
+                Total = Total + Suma;
+                f = 0;
+                c++;
+                i++;
+            }
+
+            VectorDatos[9] = Total;
+
+            txtComidas.Text = Convert.ToString(VectorDatos[5]);
+            txtBebidasSinAlc.Text = Convert.ToString(VectorDatos[6]);
+            txtBebidasConAlc.Text = Convert.ToString(VectorDatos[7]);
+            txtPostres.Text = Convert.ToString(VectorDatos[8]);
+            txtTotal.Text = Convert.ToString(VectorDatos[9]);
+        }
+
+        private void dtvMozos_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                e.Handled = true;
+            }
         }
     }
 }
